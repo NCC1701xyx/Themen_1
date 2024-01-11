4d2b4e3058b0d45c82383bd710ec9d68f53983ae
BehrangV <155629392+BehrangV@users.noreply.github.com>
2024-01-11 09:48:48 +0000

    06/if-else

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -1,7 +1,7 @@
 // Entscheidungsstrukturen | control flow
 
 // Deklaration + Assignment
-const ageJohn = 32;
+const ageJohn = 30;
 const ageMark = 30;
 
 // Deklaration
@@ -26,10 +26,21 @@ console.log("------------------");
 // if(true)
 // if(false)
 // if(isJohnOlder)
-if (ageJohn > ageMark)
+// if (ageJohn > ageMark)
+// {
+//     console.log ("John ist älter !");
+// }
+
+/************ IF - ELSE  ************/
+// mit Alternative  
+// entweder ja oder nein
+
+if (isJohnOlder) 
 {
-    console.log ("John ist älter !");
+    console.log ("John ist älter !"); 
+} 
+else
+{
+    console.log ("John ist jünger !");
 }
-
-
 
