476f1686ec0992c526456494eeeb865e3894383f
BehrangV <155629392+BehrangV@users.noreply.github.com>
2024-01-11 10:05:01 +0000

    06/if-else if

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -46,3 +46,25 @@ console.log("------------------");
 
 /************  Ternäre (ternary) Schreibweise ************/
 // console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+
+
+
+
+
+/************ IF - ELSE IF  ************/
+
+// mit alternativen Fällen (älter, jünger, gleich alt)
+
+// if (isJohnOlder) 
+// {
+//     console.log ("John ist älter !"); 
+// } 
+// else if(isJohnEqual)
+// {
+//     console.log ("John ist gleich alt! ")
+// }
+// else
+// {
+//     console.log ("John ist jünger !");
+// }
+
