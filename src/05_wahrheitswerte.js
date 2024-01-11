f0cacb9563f0d91fbceaff720db948d9ed47535d
BehrangV <155629392+BehrangV@users.noreply.github.com>
2024-01-11 08:03:15 +0000

    origin

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -1,38 +1,20 @@
 
-// /* Berechnung Alter */
 
-// // Deklaration
-// let ageJohn, ageMark;
-// const birthYearJohn = 2000;
-// const birthYearMark = 1990;
+      /* Berechnung Alter */
 
-// // Berechnung Alter
-// let date = new Date(); // Systemfunktion Datum
-// let year = date.getFullYear(); // Daten liefern lassen
-// ageJohn = year - birthYearJohn;
-// ageMark = year - birthYearMark;
+// Deklaration
+let ageJohn, ageMark;
+const birthYearJohn = 2000;
+const birthYearMark = 1990;
 
-// // Ausgabe
-// console.log("ageJohn: " + ageJohn);
-// console.log("ageMark: " + ageMark);
-// console.log("Datum: " + date);
-const prompt = require('prompt-sync')({sigint: true});
-// Geburtsdatum abfragen und Alter anzeigen
-function alterBerechnen(geburtsdatum) {
-    let geburtsdatumObjekt = new Date(geburtsdatum);
-    let aktuellesDatum = new Date();
-    let differenzJahre = aktuellesDatum.getFullYear() - geburtsdatumObjekt.getFullYear();
+// Berechnung Alter
+let date = new Date(); // Systemfunktion Datum
+let year = date.getFullYear(); // Daten liefern lassen
+ageJohn = year - birthYearJohn;
+ageMark = year - birthYearMark;
 
-    if (aktuellesDatum.getMonth() < geburtsdatumObjekt.getMonth() ||
-        (aktuellesDatum.getMonth() === geburtsdatumObjekt.getMonth() &&
-        aktuellesDatum.getDate() < geburtsdatumObjekt.getDate())) {
-        differenzJahre--;
-    }
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("Datum: " + date);
 
-    return differenzJahre;
-}
-
-// Beispielaufruf
-let geburtsdatum = prompt("Geben Sie Ihr Geburtsdatum im Format YYYY-MM-DD ein:");
-let alter = alterBerechnen(geburtsdatum);
-console.log("Ihr Alter beträgt " + alter + " Jahre.");
