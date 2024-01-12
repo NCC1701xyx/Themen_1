93a509e167966d72b1c58f172e66a976494ebf01
BehrangV <155629392+BehrangV@users.noreply.github.com>
2024-01-12 09:47:16 +0000

    korrektur

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -2,11 +2,40 @@
 // 1. Kapselung von Codeblöcken
 
 //Funktionsaufruf/ Call
-test();
+// test();
 
 // Funktionsrumpf/ Funktionsdeklaration/ callee
 
-function test()
-{
-    console.log("Hallo, Benjamin!") 
+// function test()
+// {
+//     console.log ("Hallo, Benjamin! ");
+// }
+// /***** Funktionen 02a *****/
+// // 2a. Parametrisierung + Datenübergabe von INNEN
+
+
+// // call
+// ausgabeNamen(),
+// //Funktion
+//   function ausgabeNamen()
+// {
+//     //interne variable
+//     let firstName = Maria
+//     console.log ("Hallo, " + firstName + "!");
+    
+// }
+// //console.log(firstName);// fehler : scope!
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// Funktionsaufruf
+// ausgabeNamen();
+
+// Funktion
+function ausgabeNamen() {
+    // Interne Variable
+    let firstName = "Maria"; // Strings müssen in Anführungszeichen stehen
+    console.log("Hallo, " + firstName + "!");
+    //console.log(firstName); // Das führt zu einem Fehler, da firstName außerhalb des Gültigkeitsbereichs (Scope) dieser Funktion ist
 }
