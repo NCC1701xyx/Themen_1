6bcb7c859e1e56158b5f661be61dc0e68c159b9f
BehrangV <155629392+BehrangV@users.noreply.github.com>
2024-01-12 09:18:28 +0000

    Funktionsaufruf

---
+++ src/08_funktionen_I.js
@@ -0,0 +1,12 @@
+/***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+//Funktionsaufruf/ Call
+test();
+
+// Funktionsrumpf/ Funktionsdeklaration/ callee
+
+function test()
+{
+    console.log("Hallo, Benjamin!") 
+}
