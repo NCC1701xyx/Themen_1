63756b142f316e7484dcbe64981b264bee23ce57
BehrangV <155629392+BehrangV@users.noreply.github.com>
2024-01-15 09:23:51 +0000

    grundrechenarten funktionen

---
+++ src/09_funktione_II.js
@@ -0,0 +1,50 @@
+
+
+/*** Rechner */
+/*
+0. a+b | a-b | a*b | a/b  --> Ergebnis c 
+
+1. Dateneingabe + -überprüfung :  
+2. Auswahl Rechenart : 
+3. Fkt. Grundrechenarten : 
+4. Ausgabe in Konsole : 
+*/
+// Addition
+function addition(a, b) {
+	return a + b;
+  }
+  
+  // Subtraktion
+  function subtraktion(a, b) {
+	return a - b;
+  }
+  
+  // Multiplikation
+  function multiplikation(a, b) {
+	return a * b;
+  }
+  
+  // Division
+  function division(a, b) {
+	if (b !== 0) {
+	  return a / b;
+	} else {
+	  return "Division durch Null ist nicht definiert";
+	}
+  }
+  
+ 
+
+
+
+
+
+
+
+// module: output | test:
+// output("hello");
+// output(2);
+function output(outputData) {
+	console.log(outputData);
+}
+  
