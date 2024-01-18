f853b5f9e4850f8725a2ec29a1885fb4849c5588
BehrangV <155629392+BehrangV@users.noreply.github.com>
2024-01-18 08:22:21 +0000

    variation

--- src/12_Codehallenge_III.js
+++ src/12_Codehallenge_III.js
@@ -13,41 +13,79 @@
 
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
-output(getSentence(["Ich","bin","Max"],"S"));
-output(getSentence(["Bist","du","Max"],"Q"));
-output(getSentence(["Ich","bin"],"E"));
-output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
-function getSentence(arr,op) {
-    const GAP = " ";
-    const PUNCT = getPunct(op);  // . ? !
-    let str = ""; 
+// output(getSentence(["Ich","bin","Max"],"S"));
+// output(getSentence(["Bist","du","Max"],"Q"));
+// output(getSentence(["Ich","bin"],"E"));
+// output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+// function getSentence(arr,op) {
+//     const GAP = " ";
+//     const PUNCT = getPunct(op);  // . ? !
+//     let str = ""; 
 
-    for (let i = 0; i < arr.length; i++) {
-        if (i != arr.length-1) {
-            str += arr[i] + GAP;
-        } else {
-            str += arr[i] + PUNCT; 
-        }
-    }
-     return str;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (i != arr.length-1) {
+//             str += arr[i] + GAP;
+//         } else {
+//             str += arr[i] + PUNCT; 
+//         }
+//     }
+//      return str;
+// }
+
+// function getPunct(op) {
+//     switch (op) {
+//         case "S":
+//            return "."; 
+//         case "Q":
+//             return "?";
+//         case "E":
+//             return "!";
+//         default:
+//             return "#";
+//     }
+// }
+
+
+// // Modul: Ausgabe in Konsole : Test
+// // output("hi"); 
+// function output(outputData) {
+//     console.log(outputData);
+// }
+
+
+//V2
+/*** Funktion mit Array als Parameter */
+// mögliche Tests:
+output(getSentence(["Ich", "bin", "Max"], "S"));
+output(getSentence(["Bist", "du", "Max"], "Q"));
+output(getSentence(["Ich", "bin"], "E"));
+output(getSentence(["Ich", "hätt", "gern", "die", "Platt", "von", "dene", "zwei", "diwodaso", "Spass", "mache,", "habbe", "Sie", "die"], "Q"));
+
+function getSentence(arr, op) {
+    const GAP = " ";
+    const PUNCT = getPunct(op);
+    return arr.join(GAP) + PUNCT;
 }
+
+// function getPunct(op) {
+//     switch (op) {
+//         case "S":
+//             return ".";
+//         case "Q":
+//             return "?";
+//         case "E":
+//             return "!";
+//         default:
+//             return "#";
+//     }
+// }
 
 function getPunct(op) {
-    switch (op) {
-        case "S":
-           return "."; 
-        case "Q":
-            return "?";
-        case "E":
-            return "!";
-        default:
-            return "#";
-    }
+    return { "S": ".", "Q": "?", "E": "!" }[op] || "#";
 }
 
-
 // Modul: Ausgabe in Konsole : Test
-// output("hi"); 
+// output("hi");
 function output(outputData) {
     console.log(outputData);
 }
