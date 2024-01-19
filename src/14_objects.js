ebf0e9619c5568d77c44f91b2178c040cc412954
BehrangV <155629392+BehrangV@users.noreply.github.com>
2024-01-19 09:39:09 +0000

    deeper

--- src/14_objects.js
+++ src/14_objects.js
@@ -30,31 +30,58 @@ let arr;
 
 /***** Objekte 1 Daten/Funktionen *******/
 
-let person = {
-                firstName:"Behrang",
-                familyName:"Valadkhani",
-                salary: [120000, 160000],
-                permission: true,
-                sayHello: function(){
-                    return "Hallo,ich bin " + this.firstName;
+// let person = {
+//                 firstName:"Behrang",
+//                 familyName:"Valadkhani",
+//                 salary: [120000, 160000],
+//                 permission: true,
+//                 sayHello: function(){
+//                     return "Hallo,ich bin " + this.firstName;
+//                 }
+//             };
+
+
+// // output(person);
+// // output(person.familyName)
+// // output(person.firstName)
+// // output(person.salary[1])
+// // output(person.permission)
+// // output(person.sayHello())
+
+// // const txt = person.sayHello() + ", ich bin " + person.firstName +
+//             // " und verdiene " + person.salary[1] + " p.a."
+
+//  const txt = person.sayHello() +
+//             " und verdiene " + person.salary[1] + " p.a."
+
+// output(txt);
+
+
+
+
+/***** Objekte 2 - Hierarchie *******/
+// Tiefer, tiefer - Irgendwo in der Tiefe gibt es ein Licht // K.Bush
+// Der Baikalsee ist mit 1642 m der tiefste See der Erde. ...
+// deep - deeper - deepest
+// nested object {{{}}}
+
+let baikal = {
+                value:"10m",
+                deep:{
+                    deeper:{
+                        temperature: "4 Grad Celsius",
+                        deepest: "Das Licht - auf 1642m!"
+                    }
                 }
-            };
 
 
-// output(person);
-// output(person.familyName)
-// output(person.firstName)
-// output(person.salary[1])
-// output(person.permission)
-// output(person.sayHello())
 
-// const txt = person.sayHello() + ", ich bin " + person.firstName +
-            // " und verdiene " + person.salary[1] + " p.a."
+             }
 
- const txt = person.sayHello() +
-            " und verdiene " + person.salary[1] + " p.a."
-
-output(txt);
+output(baikal);
+output(baikal.value);
+output(baikal.deep.deeper.temperature);
+output(baikal.deep.deeper.deepest);
 
 
 
